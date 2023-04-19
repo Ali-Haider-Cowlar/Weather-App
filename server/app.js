a1f677d43aa8418481c22835829f131f2f1fc366
Ali Haider Randhawa <130780970+Ali-Haider-Cowlar@users.noreply.github.com>
2023-04-19 17:44:40 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ connectDB();
 const cors = require("cors");
 const app = express();
 
-// Middlewares
+//Middlewares
 app.use(express.json());
 app.use(cors());
 
@@ -27,19 +27,22 @@ app.use("/user", userRoutes); // localhost:5000/user
 //   port: config.influx.port,
 //   database: config.influx.database,
 // });
+// const influx = new Influx.InfluxDB({
+//   host: config.influx.host,
+//   port: config.influx.port,
+//   database: config.influx.database,
+// });
 app.get("/api/weather", async (req, res) => {
   const { longitude, latitude } = req.query;
 
   try {
     const { data } = await axios.get(
-      `http://api.weatherapi.com/v1/current.json?key=${config.apiKey}&q=${latitude},${longitude}`
+      `http://api.weatherapi.com/v1/current.json?key=${config.apiKey}&q=${latitude},${longitude}``http://api.weatherapi.com/v1/current.json?key=${config.apiKey}&q=${latitude},${longitude}`
     );
-
     // Write weather data to InfluxDB
     // await influx.writePoints([
     //   {
     //     measurement: "weather",
-
     //     fields: {
     //       name: data.location.name,
     //       temperature: data.current.temp_c,
@@ -57,10 +60,7 @@ app.get("/api/weather", async (req, res) => {
       temperature: data.current.temp_c,
       condition: data.current.condition.text,
       pressure: data.current.pressure_mb,
-
-      wind_speed: data.current.wind_kph,
-      humidity: data.current.humidity
-
+      wind_speed: data.current.wind_mph,
     });
   } catch (error) {
     console.error(error);
@@ -69,8 +69,16 @@ app.get("/api/weather", async (req, res) => {
 });
 
 //Database Connection
-mongoose.connection.once("open", () => {
-  app.listen(PORT, () =>
-    console.log(`Connected to MongoDB: Server running on port ${PORT}`)
-  );
-});
+
+async function startServer() {
+  await mongoose.connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
+
+app.listen(PORT, () =>
+  console.log(`Connected to MongoDB: Server running on port ${PORT}`)
+);
+
+module.exports = { app, startServer };
